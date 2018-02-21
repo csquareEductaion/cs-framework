7b442032441d3c5f57e2868687fb23874fb087da
Vidya Sagar <v-vsagar@funko.com>
2018-02-21 10:26:44 +0530

    Moved to property

--- cs-common-util/src/main/java/com/csquare/framework/util/PropertyUtil.java
+++ cs-common-util/src/main/java/com/csquare/framework/util/PropertyUtil.java
@@ -10,11 +10,50 @@ import com.csquare.framework.util.SystemUtil.SystemKey;
 
 
 public enum PropertyUtil {
-    INSTANCE;
+
+    /* API_GATEWAY */
+    API_GATEWAY("cs-api-gateway-app.properties"),
+    /* LEAD_MGT */
+    LEAD_MGT("cs-lead-mgt-app.properties"),
+    /* STUDENT_MGT */
+    STUDENT_MGT("cs-student-mgt-app.properties"),
+    /* TUTOR_MGT */
+    TUTOR_MGT("cs-tutor-mgt-app.properties"),
+    /* REF_MGT */
+    REF_MGT("cs-ref-mgt-app.properties"),
+    /* USER_MGT */
+    USER_MGT("cs-user-mgt-app.properties"),
+    /* COMMUNICATION_MGT */
+    COMMUNICATION_MGT("cs-communication-mgt-app.properties");
 
     private Properties prop = new Properties();
+    /** value **/
+    private final String value;
 
-    private PropertyUtil() throws RuntimeException {
+    private PropertyUtil(String value) throws RuntimeException {
+        this.value = value;
+    }
+
+    /**
+     * Method to provide region on which application is hosted. Every system
+     * must set CodeXRegion as parameter with value like eu-west-1.amazonaws.com
+     *
+     * @param key
+     *            - The String
+     * @return value - The String
+     */
+    public void init() {
+
+        String fileName = this.value;
+        InputStream input = null;
+        try {
+            input = getPropertyIS(fileName);
+            prop.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            CommonUtil.close(input);
+        }
 
     }
 
@@ -28,28 +67,6 @@ public enum PropertyUtil {
             throw new RuntimeException(e);
         }
         return input;
-    }
-
-    /**
-     * Method to provide region on which application is hosted. Every system
-     * must set CodeXRegion as parameter with value like eu-west-1.amazonaws.com
-     *
-     * @param key
-     *            - The String
-     * @return value - The String
-     */
-    public void init(String fileName) {
-
-        InputStream input = null;
-        try {
-            input = getPropertyIS(fileName);
-            prop.load(input);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            CommonUtil.close(input);
-        }
-
     }
 
     /**
